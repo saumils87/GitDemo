2d59d3021f30ea3bdfac21da4edd904d027ebf69
saumil <saumils87@gmail.com>
2020-11-28 20:36:06 +0530

    this is commit from it

--- src/test/java/qaclickacademy/SeleniumTest.java
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -6,6 +6,8 @@ public class SeleniumTest {
 	@Test
 	public void Browser() {
 		System.out.println("hello world");
+	System.out.println("hello");
+		System.out.println("hello1");
 	}
 	//@Test
 	public void Ui() {
