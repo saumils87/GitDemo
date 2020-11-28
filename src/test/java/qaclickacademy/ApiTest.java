65017c7964bfd0e1a8a04efb8f4206bd6a853e51
saumil <saumils87@gmail.com>
2020-11-28 19:05:09 +0530

    first commit

---
+++ src/test/java/qaclickacademy/ApiTest.java
@@ -0,0 +1,11 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class ApiTest {
+	@Test
+	public void Api() {
+		System.out.println("hello world1");
+	}
+
+}
---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,15 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	@Test
+	public void Browser() {
+		System.out.println("hello world");
+	}
+	//@Test
+	public void Ui() {
+		System.out.println("please be the ui developer");
+	}
+
+}
